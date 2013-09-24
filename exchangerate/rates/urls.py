ba3d41c135b50f83d0683750459f9aafd9ca1f9b
Pavel Vavruska <vavruska.pavel@gmail.com>
2013-09-24 16:46:17 +0200

    Add filtering of data by year, month and day

--- exchangerate/rates/urls.py
+++ exchangerate/rates/urls.py
@@ -5,6 +5,8 @@ from rates import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<pk>\d{1,2})/$', views.DetailView.as_view(), name='detail'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[^\.]+)/$', views.DetailView.as_view(), name='detail1'),
+    url(r'^(?P<year>\d{4})/$', views.IndexView.as_view(), name='year'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/$', views.IndexView.as_view(), name='month'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$', views.IndexView.as_view(), name='day'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[^\.]+)/$', views.DetailView.as_view(), name='detail'),
 )
--- exchangerate/rates/views.py
+++ exchangerate/rates/views.py
@@ -8,8 +8,15 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_rates_list'
 
     def get_queryset(self):
+        query = ExchangeRate.objects.order_by('-date')
+        if 'year' in self.kwargs:
+            query = query.filter(date__year=self.kwargs['year'])
+            if 'month' in self.kwargs:
+                query = query.filter(date__month=self.kwargs['month'])
+                if 'day' in self.kwargs:
+                    query = query.filter(date__day=self.kwargs['day'])
         """Return the last five published polls."""
-        return ExchangeRate.objects.order_by('-date')[:5]
+        return query
 
 
 class DetailView(generic.DetailView):
