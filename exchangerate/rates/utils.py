3efa1cb636c22fe8f2943a7efbc25f2286140e3b
Pavel Vavruska <vavruska.pavel@gmail.com>
2013-09-25 23:45:21 +0200

    Add draft of utils for datamining

---
+++ exchangerate/rates/utils.py
@@ -0,0 +1,8 @@
+def get_currencies(date):
+    """Returns a list of currencies in a particular date."""
+    return
+
+
+def get_exchange_rates(date):
+    """Returns exchange rates for currencies in a particular date."""
+    return
