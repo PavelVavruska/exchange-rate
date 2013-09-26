fb4cefb2d438187102c646037f51096c3ddd5dc0
Pavel Vavruska <vavruska.pavel@gmail.com>
2013-09-26 23:03:02 +0200

    Add empty functions for datamining

--- exchangerate/rates/utils.py
+++ exchangerate/rates/utils.py
@@ -1,8 +1,22 @@
-def get_currencies(date):
+def get_url(date):
+    """Returns full url with correct data format."""
+    url = ""
+    return url
+
+
+def get_raw_data(url):
+    """Returns data from url as list of strings."""
+    raw_data = ['', '', '']
+    return raw_data
+
+
+def get_currencies(raw_data):
     """Returns a list of currencies in a particular date."""
-    return
+    data = ['', '', '']
+    return data
 
 
-def get_exchange_rates(date):
+def get_exchange_rates(raw_data):
     """Returns exchange rates for currencies in a particular date."""
-    return
+    data = ['', '', '']
+    return data
