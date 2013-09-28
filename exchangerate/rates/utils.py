52c2842e6b43a343d1a84f3e988e17b23d61737d
Pavel Vavruska <vavruska.pavel@gmail.com>
2013-09-28 02:33:47 +0200

    Fix test input string and add pass function

--- exchangerate/rates/utils.py
+++ exchangerate/rates/utils.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 def get_url(date):
     """Returns full url with correct data format."""
     url = ""
@@ -12,8 +14,17 @@ def get_raw_data(url):
 
 def get_currencies(raw_data):
     """Returns a list of currencies in a particular date."""
-    data = ['', '', '']
-    return data
+    line_number = 0
+    data_array = []
+    content = raw_data.splitlines()
+    for line_of_text in content:
+        line_number += 1
+        # FIND NEEDED HTML ROW
+        if line_number > 2:
+            line_array = dedent(line_of_text).split('|')
+            actual_currency = {'name': line_array[1], 'country': line_array[0], 'code': line_array[3]}
+            data_array.append(actual_currency)
+    return data_array
 
 
 def get_exchange_rates(raw_data):
